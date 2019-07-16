52080133e427191431a508879de6e8f4c4285a24
rednjzz <rednjzz@gmail.com>
2019-07-16 15:47:05 +0900

    add router

---
+++ src/components/HomeHeadder.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="flexContainer">
+      <img class="iconContainer flexItem" src="@/assets/baseline-tune-24px.svg">
+      <span class="flexItem headderTitle">Featured</span>
+      <img class="iconContainer flexItem" src="@/assets/baseline-search-24px.svg"> 
+    </div>
+    <div>
+      <span class="flexItem headderSubTitle">RATING</span>
+      <img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.headderTitle {
+  color: #5887F9;
+  font-size: 30px;
+  font-weight: 900;
+}
+.headderSubTitle {
+  font-size: 15px;
+  font-weight: 800;
+}
+.flexContainer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.flexItem {
+
+}
+.iconContainer {
+  width: 30px;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.addContainer {
+  float: left;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+</style>
---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import router from 'vue-router'
+import HomeView from '@/views/Home.vue'
+
+Vue.use(router);
+
+export default new router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
+  ]
+})
+
+
